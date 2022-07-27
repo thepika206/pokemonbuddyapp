55e0044778c65cf5a8dfa2685a3148eeb2cef29f
thepika206 <109635893+thepika206@users.noreply.github.com>
2022-07-26 21:52:42 -0500

    added missing semi-colons for clean up

--- app.js
+++ app.js
@@ -12,33 +12,33 @@ function isP1KO() {
 
     }
     else {
-        const remainingHP = p1HP - p1Dmg
-        console.log(`pokemon 1 has ${remainingHP} HP left`)
+        const remainingHP = p1HP - p1Dmg;
+        console.log(`pokemon 1 has ${remainingHP} HP left`);
         document.querySelector('#p1Card').style.backgroundColor = 'white';
         document.querySelector('#p1CardHeader').innerText = 'Pokemon 1';
     }
 }
 
 
-let pokemon1HPDisplay = document.querySelector('#p1TotalHP')
+let pokemon1HPDisplay = document.querySelector('#p1TotalHP');
 pokemon1HPDisplay.addEventListener('input', function () {
     p1HP = pokemon1HPDisplay.value;
     console.log(`pokemon1 hp set to ${p1HP}`)
 })
 
 
-let incBtn = document.querySelector('#p1IncDmg')
+let incBtn = document.querySelector('#p1IncDmg');
 incBtn.addEventListener('click', function () {
     if (p1HP > 0) {
         p1Dmg += 10;
         console.log(`damage is now ${p1Dmg}`);
-        let damage = document.querySelector('span.damage')
+        let damage = document.querySelector('span.damage');
         damage.innerText = p1Dmg;
         isP1KO();
     }
     else {
-        console.error('no damage, hp is not set')
-        alert('Set the HP before adding damage')
+        console.error('no damage, hp is not set');
+        alert('Set the HP before adding damage');
     }
 })
 
@@ -47,36 +47,36 @@ incBtnFifty.addEventListener('click', function () {
     if (p1Dmg > 0) {
         p1Dmg += 50;
         console.log(`damage is now ${p1Dmg}`);
-        let damage = document.querySelector('span.damage')
+        let damage = document.querySelector('span.damage');
         damage.innerText = p1Dmg;
         isP1KO();
     }
     else {
         console.error('no damage, hp is not set');
-        alert('Set the HP before adding damage')
+        alert('Set the HP before adding damage');
     }
 
 })
 
-let decBtn = document.querySelector('#p1DecDmg')
+let decBtn = document.querySelector('#p1DecDmg');
 decBtn.addEventListener('click', function () {
     if (p1Dmg > 0) {
         p1Dmg -= 10;
         console.log(`damage is now ${p1Dmg}`);
-        let damage = document.querySelector('#p1DamageDisplay')
+        let damage = document.querySelector('#p1DamageDisplay');
         damage.innerText = p1Dmg;
         isP1KO();
     }
     else {
-        console.error('damage cannot be less than zero')
+        console.error('damage cannot be less than zero');
     }
 })
 
-let clrBtn = document.querySelector('#p1ClrDmg')
+let clrBtn = document.querySelector('#p1ClrDmg');
 clrBtn.addEventListener('click', function () {
     p1Dmg = 0;
     console.log(`damage is now ${p1Dmg}`);
-    let damage = document.querySelector('span.damage')
+    let damage = document.querySelector('span.damage');
     damage.innerText = p1Dmg;
     isP1KO();
 })
